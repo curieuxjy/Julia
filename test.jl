50a57ef31256da407fdb4d9bfcd11f91f92d9e5b
Jungyeon Lee <skyfiower9721@gmail.com>
2020-05-18 21:54:01 +0900

    feat: add chapter3 code

---
+++ test.jl
@@ -0,0 +1,15 @@
+function printlyrics()
+    println("Hello, World.")
+    println("Nice to meet you.")
+end
+
+
+
+function repeatlyrics()
+    printlyrics()
+    printlyrics()
+end
+
+
+
+repeatlyrics()
